4fa1c34d26007071c4580c2ddc559f7d918b8e4e
Sai Teja Kavuru <52291943+saitejakavuru@users.noreply.github.com>
2021-06-16 20:17:06 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,15 +3,27 @@
 
 ## Write a short comment describing this function
 
+## Creates a special matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-           m <- NULL
+        ## Initialize the inverse property
+        m <- NULL
+           ## Method to set the matrix
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
-        get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
+        ## Method the get the matrix
+        get <- function()
+                   ## Return the matrix
+                   x
+        ## Method to set the inverse of the matrix
+        setmean <- function(mean) 
+                   m <<- mean
+        ## Method to get the inverse of the matrix   
+        getmean <- function() 
+                   m
+        
+        ## Return a list of the methods
         list(set = set, get = get,
              setmean = setmean,
              getmean = getmean)
@@ -19,15 +31,20 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Compute the inverse of the special matrix returned by "makeCacheMatrix"
+## above. If the inverse has already been calculated then the "cachesolve" should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
         m <- x$getmean()
+        ## Just return the inverse if its already set
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
+        ## Get the matrix from our object
         data <- x$get()
+        ## Calculate the inverse using matrix multiplication
         m <- mean(data, ...)
         x$setmean(m)
         m
